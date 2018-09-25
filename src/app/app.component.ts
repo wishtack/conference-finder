9f17842a95f9083b4adf14f0198947e5de28fea5
Younes JAAIDI <yjaaidi@gmail.com>
2018-09-25 12:19:10 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'wishtack-angular-interchangeable-components-demo';
+}
