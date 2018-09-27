b4d84d542568dda316c719dde618b8d21f26b66a
Younes JAAIDI <yjaaidi@gmail.com>
2018-09-27 23:50:47 +0200

    Show more conference information in information list.

--- src/app/conference/conference.ts
+++ src/app/conference/conference.ts
@@ -5,12 +5,37 @@
  * $Id: $
  */
 
+import * as moment from 'moment';
+import { Moment } from 'moment';
+
 export class Conference {
 
+    city: string;
+    country: string;
     name: string;
+    twitter: string;
+    url: string;
+
+    startDate: Moment;
+    endDate: Moment;
+
+    cfpUrl: string;
+    cfpEndDate: Moment;
 
     constructor(args: Partial<Conference> = {}) {
+
+        this.city = args.city;
+        this.country = args.country;
         this.name = args.name;
+        this.twitter = args.twitter;
+        this.url = args.url;
+
+        this.startDate = args.startDate != null ? moment(args.startDate) : null;
+        this.endDate = args.endDate != null ? moment(args.endDate) : null;
+
+        this.cfpUrl = args.cfpUrl;
+        this.cfpEndDate = args.cfpEndDate != null ? moment(args.cfpEndDate) : null;
+
     }
 
 }
