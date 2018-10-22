9547d1816ae6ebfcc9f804b413e743a56ddbd538
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-22 15:27:25 +0200

    Control theme programmatically.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
     selector: 'wt-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    constructor(
+        @Inject(DOCUMENT) private _document: Document,
+        private _renderer: Renderer2
+    ) {
+    }
+
+    ngOnInit() {
+        this._renderer.setAttribute(this._document.body, 'class', '');
+        this._renderer.addClass(this._document.body, 'wt-dark-theme');
+    }
+
 }
